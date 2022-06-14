14854e08a426cfaf95b5e7204d747b9072c321d0
Martin Petrov <martin.petrov@nordcloud.com>
2022-06-14 22:25:42 +0300

    Staging (#5)
    * Change pipelane stage name
    
    * Try deploying only staging
    
    * Change pipeline branch
    
    * Change pipelane stage name
    
    * Add prod stage

--- backend/src/pipeline-stack.ts
+++ backend/src/pipeline-stack.ts
@@ -2,6 +2,8 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
+import { AppStage } from '~/app-stage';
+
 const connectionArn =
   'arn:aws:codestar-connections:eu-central-1:791346621844:connection/5d269634-09ef-43bc-9a8f-d7529fb2d4ab';
 const repo = 'martpet/trip-pics';
@@ -11,14 +13,23 @@ export class PipelineStack extends Stack {
     super(scope, id, props);
 
     const synthStep = new ShellStep('Synth', {
-      input: CodePipelineSource.connection(repo, 'staging', { connectionArn }),
+      input: CodePipelineSource.connection(repo, 'main', { connectionArn }),
       commands: ['npm ci -f', 'npm run synth'],
       primaryOutputDirectory: 'backend/cdk.out',
     });
 
-    new CodePipeline(this, `Pipeline-prod`, {
+    const pipeline = new CodePipeline(this, `Pipeline-prod`, {
       pipelineName: 'prod',
       synth: synthStep,
     });
+
+    const stage = new AppStage(this, 'prod', {
+      env: {
+        account: '766373560006',
+        region: 'u-central-1',
+      },
+    });
+
+    pipeline.addStage(stage);
   }
 }
