cf0306b61115a2b9bf9c410a3879b273308631c3
Martin Petrov <martin.petrov@nordcloud.com>
2022-06-16 09:34:36 +0300

    Reorganize consts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,35 +1,15 @@
 import { App } from 'aws-cdk-lib';
 
 import { AppStack } from '~/app-stack';
-import {
-  appName,
-  pipelineAccount,
-  productionAccount,
-  region,
-  stagingAccount,
-} from '~/consts';
+import { appName, pipelineEnv, prodPipelineProps, stagingPipelineProps } from '~/consts';
 import { PipelineStack } from '~/pipeline-stack';
 
 const app = new App();
 
 new PipelineStack(app, 'pipeline', {
   stackName: `${appName}-PipelineStack`,
-  env: {
-    account: pipelineAccount,
-    region,
-  },
-  pipelines: [
-    {
-      envName: 'production',
-      sourceBranch: 'main',
-      account: productionAccount,
-    },
-    {
-      envName: 'staging',
-      sourceBranch: 'staging',
-      account: stagingAccount,
-    },
-  ],
+  env: pipelineEnv,
+  pipelines: [prodPipelineProps, stagingPipelineProps],
 });
 
 new AppStack(app, 'dev', {
